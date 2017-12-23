d473933ee6b3caafa5737e09e11cb4bf8563f97d
Cyrille Berliat <cyrille@berliat.fr>
2017-12-23 23:27:25 +0000

    Adding support & sjell files for static content.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :collaborer, :manifeste, :association, :proprieteintellectuelle, :confidentialite, :contact]
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :words
+
+  scope controller: :pages do
+    get :home
+    get :collaborer
+    get :manifeste
+    get :association
+    get :proprieteintellectuelle
+    get :confidentialite
+    get :contact
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
