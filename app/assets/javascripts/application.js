23e914b0be7b635a2a873cb0c18f6f0a06cd4a20
Cyrille Berliat <cyrille@berliat.fr>
2017-12-24 04:47:33 +0000

    Integrating the menu and implementing the toggle+responsiveness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,21 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(".sidebar-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
+
+$(document).ready(function(){
+  var changeWidth = function(){
+    if ( $(window).width() < 576 ){
+    $('#wrapper').removeClass('toggled');
+    } else {
+      $('#wrapper').addClass('toggled');
+    }
+  };
+
+  $(window).resize(changeWidth);
+  changeWidth();
+});
